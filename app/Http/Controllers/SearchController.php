3159fb7a034b87d696ab9749acf1bc8ff88c21a9
DidinOS9 <didinoktaviandasetiawan@gmail.com>
2023-02-08 21:39:55 +0700

    menambah atribut search

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -7,9 +7,11 @@ use Illuminate\Http\Request;
 
 class SearchController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        $attractions = Attraction::all();
+        $attractions = Attraction::where('tempat_wisata', 'LIKE', '%'.$request->search.'%')
+                                ->orWhere('provinsi', 'LIKE', '%'.$request->search.'%')
+                                ->orWhere('jumlah_pengunjung', 'LIKE', '%'.$request->search.'%')->get();
         return view('welcome', compact('attractions'));
     }
 }
